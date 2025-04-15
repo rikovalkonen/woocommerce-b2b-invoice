7e4e0635cfcc0f483f3cf6e81b6819f6ea01994e
Riko Valkonen <riko.valkonen@gmail.com>
2025-04-15 19:57:04 +0300

    Removed console.log

--- includes/translations.php
+++ includes/translations.php
@@ -7,7 +7,5 @@ function woocommerce_b2b_invoice_load_textdomain()
         false,
         dirname(plugin_basename(__FILE__)) . '/languages/'
     );
-
-    error_log(__('Company name', 'woocommerce-b2b-invoice'));
 }
 add_action('init', 'woocommerce_b2b_invoice_load_textdomain');
